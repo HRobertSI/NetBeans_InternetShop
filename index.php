a5cc1b5928b5ad3166ee128f167cd19b9d293ca7
HRobertSI <hrobert25@hotmail.com>
2021-04-19 14:08:11 +0200

    Prilagodil MySQL.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 require_once './connection.php';
 
 $link = connectDatabase();
-$query = "SELECT * FROM data WHERE BookID = 2";
+$query = "SELECT * FROM data";
 $results = mysqli_query($link, $query);
 
 ?>
@@ -17,12 +17,17 @@ $results = mysqli_query($link, $query);
     </head>
     <body>
         <h1>Welcome to my Internet shop</h1>
-        <h3>This are our products</h3>
+        <h3>These are our products</h3>
         <?php
-            while ($row = mysqli_fetch_array($results))
-            {
-             echo $row['title']."<br>";   
-            }
+            while ($row = mysqli_fetch_array($results, MYSQLI_ASSOC))
+            { 
+            //echo "<a href='showProduct.php?id=".$row['BookID']."'">."<strong>".$row['title']."</strong>, by".$row['authors']."</a><br>";
+            $BookID=$row['BookID'];
+            echo $BookID."<br>";
+            echo "<a href='showProduct.php?id=$BookID'>"."<strong>".$row['title']."</strong></a> by: ".$row['authors']."<br>";
+            echo $row['shortDes']."<br><br>";
+            } 
+            
             //In this example we use links with ID's of products 
         /*    foreach ($productsList as $key => $value) {
                 $title = $value['title'];
